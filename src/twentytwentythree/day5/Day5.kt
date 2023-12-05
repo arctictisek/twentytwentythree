d9512a0f38c7d9157f9bde89118483fc5a8ef7c2
Frantisek Zacek <frantisek.zacek@imgarena.com>
2023-12-05 22:26:03 +0000

    2023 - Day 5 - Part 2 Smart brute force

--- src/twentytwentythree/day5/Day5.kt
+++ src/twentytwentythree/day5/Day5.kt
@@ -1,82 +1,111 @@
 package twentytwentythree.day5
 
 import readFile
-import kotlin.coroutines.CoroutineContext
+import kotlin.system.measureTimeMillis
 
 class Day5 {
     fun run() {
         val input = readFile("2023-day5.txt")
         val system = parse(input)
-//        system.minLocation().let { println(it) }
-//        system.seedsRanges().forEach { println(it) }
-        system.seedsRanges().minOf { system.mapRangeToMinLocation(it) }.let { println(it) }
-        //331445006
+        system.minLocation().let { println(it) }
+        val timeInMillis = measureTimeMillis {
+            generateSequence(1L) { it + 1L }.dropWhile { !system.mappableToSeedRange(it) }.take(1).first()
+                .let { println("~~$it") }
+        }
+        println("The brute force took $timeInMillis ms")
     }
 
     private fun parse(input: List<String>) =
-            System(input[0].replace("seeds: ", "")
-                    .split(' ')
-                    .map { it.toLong() },
-                    parseSpecificMap(input, "seed-to-soil"),
-                    parseSpecificMap(input, "soil-to-fertilizer"),
-                    parseSpecificMap(input, "fertilizer-to-water"),
-                    parseSpecificMap(input, "water-to-light"),
-                    parseSpecificMap(input, "light-to-temperature"),
-                    parseSpecificMap(input, "temperature-to-humidity"),
-                    parseSpecificMap(input, "humidity-to-location"))
+        System(
+            input[0].replace("seeds: ", "")
+                .split(' ')
+                .map { it.toLong() },
+            parseSpecificMap(input, "seed-to-soil"),
+            parseSpecificMap(input, "soil-to-fertilizer"),
+            parseSpecificMap(input, "fertilizer-to-water"),
+            parseSpecificMap(input, "water-to-light"),
+            parseSpecificMap(input, "light-to-temperature"),
+            parseSpecificMap(input, "temperature-to-humidity"),
+            parseSpecificMap(input, "humidity-to-location")
+        )
 
     private fun parseSpecificMap(input: List<String>, map: String) =
-            input.asSequence().dropWhile { it != "$map map:" }
-                    .drop(1)
-                    .takeWhile { it.isNotBlank() }
-                    .map { it.split(' ') }
-                    .map { it.map { it.toLong() } }
-                    .map { Range(it[0], it[1], it[2]) }.toList()
+        input.asSequence().dropWhile { it != "$map map:" }
+            .drop(1)
+            .takeWhile { it.isNotBlank() }
+            .map { it.split(' ') }
+            .map { it.map { it.toLong() } }
+            .map { Range(it[0], it[1], it[2]) }.toList()
 }
 
 data class Range(
-        val destinationStart: Long,
-        val sourceStart: Long,
-        val length: Long
+    val destinationStart: Long,
+    val sourceStart: Long,
+    val length: Long
 ) {
     infix fun containsSource(source: Long) = sourceStart <= source && source < sourceStart + length
+    infix fun containsDestination(destination: Long) =
+        destinationStart <= destination && destination < destinationStart + length
 }
 
 data class System(
-        val seeds: List<Long>,
-        val seedToSoil: List<Range>,
-        val soilToFertilizer: List<Range>,
-        val fertilizerToWater: List<Range>,
-        val waterToLight: List<Range>,
-        val lightToTemperature: List<Range>,
-        val temperatureToHumidity: List<Range>,
-        val humidityToLocation: List<Range>,
+    val seeds: List<Long>,
+    val seedToSoil: List<Range>,
+    val soilToFertilizer: List<Range>,
+    val fertilizerToWater: List<Range>,
+    val waterToLight: List<Range>,
+    val lightToTemperature: List<Range>,
+    val temperatureToHumidity: List<Range>,
+    val humidityToLocation: List<Range>,
 ) {
     private fun getDestinationForSource(source: Long, map: List<Range>) =
-            useRangeToGetDestination(source, map.firstOrNull { it containsSource source })
+        useRangeToGetDestination(source, map.firstOrNull { it containsSource source })
+
+    private fun getSourceForDestination(destination: Long, map: List<Range>) =
+        useRangeToGetSource(destination, map.firstOrNull { it containsDestination destination })
+
 
     private fun useRangeToGetDestination(source: Long, range: Range?) =
-            if (null == range) source else
-                range.destinationStart + source - range.sourceStart
+        if (null == range) source else
+            range.destinationStart + source - range.sourceStart
 
-    private fun mapToLocation(seed: Long): Long {
-        println("Seed $seed")
-        val soil = getDestinationForSource(seed, seedToSoil)//.also { println("Soil: $it") }
-        val fertilizer = getDestinationForSource(soil, soilToFertilizer)//.also { println("Fertilize: $it") }
-        val water = getDestinationForSource(fertilizer, fertilizerToWater)//.also { println("Water: $it") }
-        val light = getDestinationForSource(water, waterToLight)//.also { println("Light: $it") }
-        val temperature = getDestinationForSource(light, lightToTemperature).also { println("Temperature: $it") }
-        val humidity = getDestinationForSource(temperature, temperatureToHumidity).also { println("Humidity: $it") }
-        return getDestinationForSource(humidity, humidityToLocation).also { println("Location: $it") }
-    }
+    private fun useRangeToGetSource(destination: Long, range: Range?) =
+        if (null == range) destination else
+            range.sourceStart + destination - range.destinationStart
 
-    fun mapRangeToMinLocation(range: Pair<Long, Long>) =
-            (range.first until range.first + range.second).minOf { mapToLocation(it) }
+    private fun mapToLocation(seed: Long) =
+        getDestinationForSource(seed, seedToSoil).let { soil ->
+            getDestinationForSource(soil, soilToFertilizer).let { fertilizer ->
+                getDestinationForSource(fertilizer, fertilizerToWater).let { water ->
+                    getDestinationForSource(water, waterToLight).let { light ->
+                        getDestinationForSource(light, lightToTemperature).let { temperature ->
+                            getDestinationForSource(temperature, temperatureToHumidity).let { humidity ->
+                                getDestinationForSource(humidity, humidityToLocation)
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
-    private fun mapAllSeedsToLocations() =
-        seeds.map { mapToLocation(it) }
+    fun mappableToSeedRange(location: Long): Boolean =
+        getSourceForDestination(location, humidityToLocation).let { humidity ->
+            getSourceForDestination(humidity, temperatureToHumidity).let { temperature ->
+                getSourceForDestination(temperature, lightToTemperature).let { light ->
+                    getSourceForDestination(light, waterToLight).let { water ->
+                        getSourceForDestination(water, fertilizerToWater).let { fertilizer ->
+                            getSourceForDestination(fertilizer, soilToFertilizer).let { soil ->
+                                getSourceForDestination(soil, seedToSoil).let { seed ->
+                                    seeds.chunked(2)
+                                        .map { it[0] to it[1] }
+                                        .any { it.first <= seed && seed < it.first + it.second }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
-    fun minLocation() = mapAllSeedsToLocations().min()
-
-    fun seedsRanges() = seeds.chunked(2).map { it[0] to it[1] }
+    fun minLocation() = seeds.minOf { mapToLocation(it) }
 }
