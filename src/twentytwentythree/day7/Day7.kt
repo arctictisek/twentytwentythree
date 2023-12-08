7efa9fb75d973ce51f535c60c7df1693a5d98bb4
Frantisek Zacek <frantisek.zacek@imgarena.com>
2023-12-08 07:32:04 +0000

    2023 - Day 7 - Part 2

--- src/twentytwentythree/day7/Day7.kt
+++ src/twentytwentythree/day7/Day7.kt
@@ -21,7 +21,7 @@ class Day7 {
 private fun Char.toCardInt(): Int {
     return if (this in setOf('J', 'Q', 'K', 'A')) {
         when (this) {
-            'J' -> 11
+            'J' -> 1
             'Q' -> 12
             'K' -> 13
             'A' -> 14
@@ -38,7 +38,7 @@ data class Game(
         val bid: Int
 ) : Comparable<Game> {
     override fun compareTo(other: Game) =
-            rankHand().compareTo(other.rankHand()).let {
+            rankHandsWithJokers().compareTo(other.rankHandsWithJokers()).let {
                 if (it != 0) it else tieBreak(other)
             }
 
@@ -82,6 +82,13 @@ data class Game(
         }
     }
 
+    private fun rankHandsWithJokers(): Rank {
+        return setOf('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A')
+                .map { hand.replace('J', it) }
+                .map { Game(it, this.bid) }
+                .maxOf { it.rankHand() }
+    }
+
     private fun tieBreak(other: Game): Int {
         val cardInts = this.hand.map { it.toCardInt() }
         val otherCardInts = other.hand.map { it.toCardInt() }
@@ -89,74 +96,6 @@ data class Game(
         return 0
     }
 
-    private fun realTieBreak(other: Game): Int {
-        if (rankHand() == Rank.FIVE) {
-            return this.hand[0].toCardInt().compareTo(other.hand[0].toCardInt())
-        } else if (rankHand() == Rank.FOUR) {
-            val quadCard = this.hand.toSet().find { uniqueCard -> this.hand.count { uniqueCard == it } == 4 }!!
-            val otherQuadCard = other.hand.toSet().find { uniqueCard -> other.hand.count { uniqueCard == it } == 4 }!!
-            return if (quadCard != otherQuadCard) quadCard.toCardInt().compareTo(otherQuadCard.toCardInt())
-            else this.hand.replace("" + quadCard, "")[0].toCardInt()
-                    .compareTo(other.hand.replace("" + quadCard, "")[0].toCardInt())
-        } else if (rankHand() == Rank.THREE) {
-            val tripCard = this.hand.toSet().find { uniqueCard -> this.hand.count { uniqueCard == it } == 3 }!!
-            val otherTripCard = other.hand.toSet().find { uniqueCard -> other.hand.count { uniqueCard == it } == 3 }!!
-            return if (tripCard != otherTripCard) tripCard.toCardInt().compareTo(otherTripCard.toCardInt())
-            else {
-                val kickers = this.hand.replace("" + tripCard, "").map { it.toCardInt() }.toSortedSet().toList().reversed()
-                val otherKickers = other.hand.replace("" + tripCard, "").map { it.toCardInt() }.toSortedSet().toList().reversed()
-                if (kickers[0] == otherKickers[0]) kickers[1].compareTo(otherKickers[1]) else kickers[0].compareTo(otherKickers[0])
-            }
-        } else if (rankHand() == Rank.BOAT) {
-            val tripCard = this.hand.toSet().find { uniqueCard -> this.hand.count { uniqueCard == it } == 3 }!!.toCardInt()
-            val otherTripCard = other.hand.toSet().find { uniqueCard -> other.hand.count { uniqueCard == it } == 3 }!!.toCardInt()
-            val pairCard = this.hand.toSet().find { uniqueCard -> this.hand.count { uniqueCard == it } == 2 }!!.toCardInt()
-            val otherPairCard = other.hand.toSet().find { uniqueCard -> other.hand.count { uniqueCard == it } == 2 }!!.toCardInt()
-            if (tripCard != otherTripCard) tripCard.compareTo(otherTripCard)
-            else pairCard.compareTo(otherPairCard)
-        } else if (rankHand() == Rank.TWO) {
-            val pairCards = this.hand.toSortedSet()
-                    .filter { uniqueCard -> this.hand.count { uniqueCard == it } == 2 }
-                    .map { it.toCardInt() }
-                    .sorted()
-                    .reversed()
-            val otherPairCards = other.hand.toSortedSet()
-                    .filter { uniqueCard -> other.hand.count { uniqueCard == it } == 2 }
-                    .map { it.toCardInt() }
-                    .sorted()
-                    .reversed()
-            return if (pairCards[0] != otherPairCards[0]) pairCards[0].compareTo(otherPairCards[0])
-            else if (pairCards[1] != otherPairCards[1]) pairCards[1].compareTo(otherPairCards[1])
-            else {
-                println(this.hand + " " + other.hand)
-                val kicker = this.hand.map { it.toCardInt() }.filter { !pairCards.contains(it) }[0]
-                val otherKicker = other.hand.map { it.toCardInt() }.filter { !otherPairCards.contains(it) }[0]
-                kicker.compareTo(otherKicker)
-            }
-        } else if (rankHand() == Rank.ONE) {
-            val pairCard = this.hand.toSortedSet()
-                    .filter { uniqueCard -> this.hand.count { uniqueCard == it } == 2 }
-                    .map { it.toCardInt() }
-                    .first()
-            val otherPairCard = other.hand.toSortedSet()
-                    .filter { uniqueCard -> other.hand.count { uniqueCard == it } == 2 }
-                    .map { it.toCardInt() }
-                    .first()
-            if (pairCard != otherPairCard) return pairCard.compareTo(otherPairCard)
-            else {
-                val kickers = this.hand.map { it.toCardInt() }.filter { it != pairCard }.sortedDescending()
-                val otherKickers = other.hand.map { it.toCardInt() }.filter { it != otherPairCard }.sortedDescending()
-                (0 until 3).forEach { if (kickers[it] != otherKickers[it]) return kickers[it].compareTo(otherKickers[it]) }
-                return 0
-            }
-        } else if (rankHand() == Rank.HIGH) {
-            val kickers = this.hand.map { it.toCardInt() }.sortedDescending()
-            val otherKickers = other.hand.map { it.toCardInt() }.sortedDescending()
-            (0 until 5).forEach { if (kickers[it] != otherKickers[it]) return kickers[it].compareTo(otherKickers[it]) }
-            return 0
-        }
-        return 0
-    }
 }
 
 enum class Rank(value: Int): Comparable<Rank> {
