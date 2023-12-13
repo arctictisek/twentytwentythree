9da5ffefafe730af397e6e4b7aa7b92a43233706
Frantisek Zacek <frantisek.zacek@imgarena.com>
2023-12-13 22:07:25 +0000

    Dat 13 - wip

--- src/twentytwentythree/day13/Day13.kt
+++ src/twentytwentythree/day13/Day13.kt
@@ -4,16 +4,35 @@ import readFileToString
 
 class Day13 {
     fun run() {
-        val input = readFileToString("day13.txt", 2023)
+//        val input = readFileToString("day13.txt", 2023)
+        val input = """
+            #.##..##.
+            ..#.##.#.
+            ##......#
+            ##......#
+            ..#.##.#.
+            ..##..##.
+            #.#.##.#.
+        """.trimIndent()
         val patterns = input.split("\n\n")
                 .map { it.split("\n") }
-
-        println("plouf")
-        "xxx".symetricalOver(4)
+        println(cols(patterns[0]))
     }
+    private fun String.safeGet(n: Int) =
+        if (n < length) this[n] else '%'
+    private fun List<String>.safeGet(n: Int) = if (n < this.size) this[n] 
+    private fun String.symmetricalOver(n: Int) =
+        (1..<n).all { this.safeGet(n - it) == this.safeGet(n + it - 1) }
 
-    private fun String.symetricalOver(n: Int): Boolean {
-        (0..<3).forEach { println(it) }
-        return true
-    }
+    private fun cols(pattern: List<String>): Int = (2..<pattern[0].length).first { symmetryIndex ->
+        pattern.all { it.symmetricalOver(symmetryIndex) }
+    } - 1
+
+    private fun rows(pattern: List<String>) =
+        (2..<pattern.size).first {
+            symmetricalOverI(pattern, it)
+        } - 1
+
+    private fun symmetricalOverI(pattern: List<String>, i: Int) =
+        (1..<i).all { pattern[i - it] == pattern[i + it - 1] }
 }
